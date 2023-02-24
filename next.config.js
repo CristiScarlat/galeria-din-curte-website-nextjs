69667fab8bbc80b7180e03fc0b35d3763e28f9ea
scarlat-trainer <78.cristi@gmail.com>
2023-02-24 15:33:01 +0200

    add image path config in next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    path: '/_next/image/', 
+  }
+
 }
 
 module.exports = nextConfig
